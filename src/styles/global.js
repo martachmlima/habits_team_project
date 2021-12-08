8b45973864c9f9c299fb9cec348b08ba36aae1df
Marta Lima <martachmlima@gmail.com>
2021-12-08 08:42:32 -0300

    style: adding color variables

--- src/styles/global.js
+++ src/styles/global.js
@@ -10,14 +10,19 @@ const GlobalStyle = createGlobalStyle`
         outline:0;
     }
     :root{
-      --branco: #FFFFFF;
-      --roxo: #B13AC6;
-      --rosa-pink: #CC68BE;
-      --rosa: #EE92B9;
-      --rosa-claro: #E7ABAB;
-      --roxo-escuro: #9320C2
-      --cinza: #C1D1E8;
-        --cinza-claro: #E0E9F8;
+      --white: #f2f2f2;
+      --purple: #DFC2EB;
+      --dark-purple: #9320C2;
+      --secondary-purple: #d8bbe3;
+      --light-purple: #E8D8F1;
+      --very-ligtht-purle: #EEE3F3;
+      --pink: #FFB8B8;
+      --light-pink:#E9D5F1;
+      --very-light-pink:#EDDFF2;
+      --dark-pink: #cc68be;
+      --fuscia: #b54bfa;
+      --gray: #CCCCCC;
+      --dark-gray: #867f7f;
       }
 
     h1, h2, h3, h4, h5, h6{
