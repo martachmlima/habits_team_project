87d0df8b69a4f79ce2fc9ba2519aa9226035ab61
Gustavo <ribeiro.gustavo95@gmail.com>
2021-12-09 19:26:11 -0300

    feat(userprovider): configuring the userProvider

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,13 @@
 import Header from "../../components/Header";
+import { useUser } from "../../providers/User";
 
 const Dashboard = () => {
+  const { signOut } = useUser();
   return (
     <div>
       <Header path="dashboard" userName="Marta Lima" />
       Dashboard
+      <button onClick={signOut}> Sair </button>
     </div>
   );
 };
