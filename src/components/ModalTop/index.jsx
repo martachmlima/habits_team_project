1fd3909d1d63a96e5b9e27e5943bcbc02de135f1
Filipe Melo <filipexpert63@gmail.com>
2021-12-09 19:13:33 -0300

    feat: creating styles and position of the components

--- src/components/ModalTop/index.jsx
+++ src/components/ModalTop/index.jsx
@@ -1,15 +1,17 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as React from "react";
+import { TextField } from "@mui/material";
+import { ButtonNav, ButtonChange } from "./styles";
 
 export default function SwipeableTemporaryDrawer({ anchor }) {
   const [state, setState] = React.useState({
@@ -17,6 +19,13 @@ export default function SwipeableTemporaryDrawer({ anchor }) {
     left: false,
     bottom: false,
     right: false,
+  });
+  const formSchema = yup.object().shape({
+    username: yup
+      .string()
+      .required("Insira novo usuário")
+      .max(18, "Máximo 18 caracters"),
+    email: yup.string().required("Insira novo E-mail").email(),
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
@@ -30,42 +39,100 @@ export default function SwipeableTemporaryDrawer({ anchor }) {
 
     setState({ ...state, [anchor]: open });
   };
-  const handleSubmit = (value) => {
-    toggleDrawer(value, false);
-    console.log("teste");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+  const handleChange = (value) => {
+    console.log(value);
   };
 
   const list = (anchor) => (
     <Box
       sx={{
-        height: 500,
-        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+        width: anchor === "top" || anchor === "bottom" ? "auto" : 500,
       }}
       role="presentation"
     >
-      <List sx={{ width: "50%", m: "0 auto" }}>
-        {["Novo usuário", "Novo E-mail"].map((text, index) => (
-          <ListItem key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <MailIcon /> : <ManageAccountsIcon />}
+      <form onSubmit={handleSubmit(handleChange)}>
+        <List>
+          {
+            <ListItem>
+              <ListItemIcon
+                sx={{ width: "100%", display: "flex", flexDirection: "column" }}
+              >
+                <TextField
+                  color="secondary"
+                  sx={{
+                    "& input:valid + fieldset": {
+                      borderColor: "white",
+                      borderWidth: 1,
+                      borderRadius: 3,
+                      height: 75,
+                    },
+                    filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
+                    bgcolor: "var(--branco)",
+                    borderRadius: 3,
+                    height: 70,
+                    mt: 3,
+                  }}
+                  fullWidth
+                  label={
+                    errors.username?.message ? (
+                      errors.username?.message
+                    ) : (
+                      <>
+                        <ManageAccountsIcon /> Novo nome de usuário
+                      </>
+                    )
+                  }
+                  error={errors.username?.message}
+                  id="fullWidth"
+                  {...register("username")}
+                />
 
-              {/* onClick={toggleDrawer(anchor, false)} */}
-            </ListItemIcon>
-            <input type="text" placeholder={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {
-          <ListItem button onClick={() => handleSubmit(anchor)}>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary={"Enviar alteração"} />
-          </ListItem>
-        }
-      </List>
+                <TextField
+                  color="secondary"
+                  sx={{
+                    "& input:valid + fieldset": {
+                      borderColor: "white",
+                      borderWidth: 1,
+                      borderRadius: 3,
+                      height: 75,
+                    },
+                    filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
+                    bgcolor: "var(--branco)",
+                    borderRadius: 3,
+                    height: 70,
+                    mt: 2,
+                  }}
+                  fullWidth
+                  label={
+                    errors.email?.message ? (
+                      errors.email?.message
+                    ) : (
+                      <>
+                        <MailIcon /> Novo nome de usuário
+                      </>
+                    )
+                  }
+                  error={errors.email?.message}
+                  id="fullWidth"
+                  {...register("email")}
+                />
+                {/* onClick={toggleDrawer(anchor, false)} */}
+              </ListItemIcon>
+            </ListItem>
+          }
+        </List>
+        <Divider />
+        <List>
+          {<ButtonChange type="submit">"Enviar alteração"</ButtonChange>}
+        </List>
+      </form>
     </Box>
   );
 
@@ -73,9 +140,9 @@ export default function SwipeableTemporaryDrawer({ anchor }) {
     <div>
       {
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <ButtonNav onClick={toggleDrawer(anchor, true)}>{anchor}</ButtonNav>
           <SwipeableDrawer
-            anchor={"top"}
+            anchor={"right"}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
             onOpen={toggleDrawer(anchor, true)}
