b49968ec741c5d3cbc80f63d192dab22142d9f05
Filipe Melo <filipexpert63@gmail.com>
2021-12-10 12:20:57 -0300

    fix: fix bug at feature mobile

--- src/components/ModalProfile/styles.js
+++ src/components/ModalProfile/styles.js
@@ -1,8 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const ConteinerNav = styled.div`
   width: 100px;
-  @media (min-width: 600px) {
+
+  ${(props) =>
+    props.open &&
+    css`
+      position: absolute;
+      width: 90vw;
+      top: 8%;
+      z-index: 2;
+    `};
+
+  @media (min-width: 750px) {
     width: 300px;
+    ${(props) =>
+      props.open &&
+      css`
+        position: absolute;
+        width: 40vw;
+        top: 10%;
+        right: 10%;
+        z-index: 2;
+      `};
   }
 `;
