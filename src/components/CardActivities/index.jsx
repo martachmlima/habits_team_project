0c0ce8dcf52b8a1b4ffa4661ac6096f557223fe1
Allan Verde <allangreen2906@gmail.com>
2021-12-15 22:29:52 -0300

    feat: :sparkles:Activity editor

--- src/components/CardActivities/index.jsx
+++ src/components/CardActivities/index.jsx
@@ -1,29 +1,36 @@
 import { Container } from "./styles";
 import BasicButtons from "../Button";
-import { useGroups } from '../../providers/Groups'
+import { useGroups } from "../../providers/Groups";
+import EditaActivite from '../EditActivite'
 
-function CardActivities( { activities } ) {
+function CardActivities({ activities }) {
   const { deleteActivities } = useGroups();
 
   return (
     <>
-      {activities && activities.map((card) => 
-        <Container key={card.id}>
-          <div className="info">
-            <h3>
-              Título: <span>{card.title}</span>
-            </h3>
-            <h3>
-              Data: <span>{card.realization_time}</span>
-            </h3>
-          </div>
-          <div className="button">
-            <BasicButtons onClick={() => deleteActivities(card.id) } >DELETAR</BasicButtons>
-          </div>
-        </Container>
-    )}
+      {activities &&
+        activities.map((card) => (
+          <Container key={card.id}>
+            <div className="info">
+              <h3>
+                Título: <span>{card.title}</span>
+              </h3>
+              <h3>
+                Data: <span>{card.realization_time}</span>
+              </h3>
+            </div>
+            <div className="button">
+              <BasicButtons onClick={() => deleteActivities(card.id)}>
+                DELETAR
+              </BasicButtons>
+              <EditaActivite id={card.id} >
+                EDITAR
+              </EditaActivite>
+            </div>
+          </Container>
+        ))}
     </>
-  )
+  );
 }
 
 export default CardActivities;
---
+++ src/components/EditActivite/index.jsx
@@ -0,0 +1,145 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import CloseIcon from "@mui/icons-material/Close";
+import BasicButtons from "../Button";
+import InputTextField from '../InputTextField'
+import { Container } from "./styles";
+import { useUser } from "../../providers/User";
+import api from "../../services/api";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import toast from "react-hot-toast";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+} from "@mui/material";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+const BootstrapDialogTitle = (props) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+function EditaActivite({ id }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const formSchema = yup.object().shape({
+    title: yup.string("Digite um novo título").required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const { token } = useUser();
+
+  const editActivitie = (data) => {
+    api
+      .patch(`activities/${id}/`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        toast.success("Edição feita com sucesso!");
+      })
+      .catch((err) => console.log(err));
+    handleClose();
+  };
+
+  return (
+    <Container>
+      <BasicButtons onClick={handleClickOpen}>Editar</BasicButtons>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleClose}
+        >
+          Editar Atividade
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <form onSubmit={handleSubmit(editActivitie)}>
+            <List>
+              <ListItem>
+                <ListItemIcon
+                  sx={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <InputTextField
+                    label='Novo título'
+                    errors={errors.title?.message}
+                    register={register}
+                    valueRegister={"title"}
+                  />
+                </ListItemIcon>
+              </ListItem>
+            </List>
+            <List>
+              <BasicButtons type="submit">Enviar edição</BasicButtons>
+            </List>
+          </form>
+        </DialogContent>
+      </BootstrapDialog>
+    </Container>
+  );
+}
+
+export default EditaActivite;
