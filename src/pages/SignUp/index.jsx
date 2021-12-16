3152ba98b36a39da6c312ea653f2675b28f59b88
Gustavo <ribeiro.gustavo95@gmail.com>
2021-12-16 10:14:17 -0300

    feat(login and signup): add feature back to home, and refact with component imput

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,16 +6,17 @@ import image from "../SignUp/SvgCabeça.png";
 import api from "../../services/api";
 import toast from "react-hot-toast";
 import * as React from "react";
-import TextField from "@mui/material/TextField";
 import {
-  Conteiner,
+  Container,
   ConteinerImg,
   ConteinerTitle,
   ConteinerForm,
   ConteinerFormGradient,
+  ContainedSvg,
   ConteinerBox,
 } from "./styles";
 import { Link } from "react-router-dom";
+import InputTextField from "../../components/InputTextField";
 
 const SignUp = () => {
   const history = useHistory();
@@ -72,152 +73,73 @@ const SignUp = () => {
   };
 
   return (
-    <Conteiner>
+    <Container>
       <ConteinerBox>
-        <ConteinerTitle>
-          <h1>Gest</h1>
-          <h2>Habit</h2>
-        </ConteinerTitle>
-        <ConteinerImg>
-          <img src={image} alt="" />
-        </ConteinerImg>
+        <ContainedSvg>
+          <h1 onClick={() => history.push("/")}>
+            Gest <span>Habit</span>
+          </h1>
+          <img src={image} alt="conexao" />
+        </ContainedSvg>
 
         <ConteinerForm>
           <ConteinerFormGradient>
             <form onSubmit={handleSubmit(handleRegister)}>
-              <TextField
-                color="secondary"
-                sx={{
-                  "& input:valid + fieldset": {
-                    borderColor: "white",
-                    borderWidth: 1,
-                    borderRadius: 3,
-                    height: 75,
-                  },
-                  filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
-                  bgcolor: "var(--branco)",
-                  borderRadius: 3,
-                  height: 70,
-                  mt: 3,
-                }}
-                fullWidth
+              <InputTextField
                 label={
                   errors.username?.message
                     ? errors.username?.message
                     : "Nome de usuário"
                 }
-                error={errors.username?.message}
-                id="fullWidth"
-                {...register("username")}
+                errors={errors.username?.message}
+                register={register}
+                valueRegister={"username"}
               />
-
-              <TextField
-                color="secondary"
-                sx={{
-                  "& input:valid + fieldset": {
-                    borderColor: "white",
-                    borderWidth: 1,
-                    borderRadius: 3,
-                    height: 75,
-                  },
-                  filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
-                  bgcolor: "var(--branco)",
-                  borderRadius: 3,
-                  height: 70,
-                  mt: 2,
-                }}
-                fullWidth
+              <InputTextField
                 label={errors.email?.message ? errors.email?.message : "Email"}
+                errors={errors.email?.message}
                 error={errors.email?.message}
-                id="fullWidth"
-                {...register("email")}
+                register={register}
+                valueRegister={"email"}
               />
-
-              <TextField
-                color="secondary"
-                sx={{
-                  "& input:valid + fieldset": {
-                    borderColor: "white",
-                    borderWidth: 1,
-                    borderRadius: 3,
-                    height: 75,
-                  },
-                  filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
-                  bgcolor: "var(--branco)",
-                  borderRadius: 3,
-                  height: 70,
-                  mt: 2,
-                }}
-                fullWidth
+              <InputTextField
                 label={
                   errors.confirmEmail?.message
                     ? errors.confirmEmail?.message
                     : "Confirme seu E-mail"
                 }
-                error={errors.confirmEmail?.message}
-                id="fullWidth"
-                {...register("confirmEmail")}
+                errors={errors.confirmEmail?.message}
+                register={register}
+                valueRegister={"confirmEmail"}
               />
 
-              <TextField
-                color="secondary"
-                sx={{
-                  "& input:valid + fieldset": {
-                    borderColor: "white",
-                    borderWidth: 1,
-                    borderRadius: 3,
-                    height: 75,
-                  },
-                  filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
-                  bgcolor: "var(--branco)",
-                  borderRadius: 3,
-                  height: 70,
-                  mt: 2,
-                }}
-                fullWidth
+              <InputTextField
                 label={
                   errors.password?.message ? errors.password?.message : "Senha"
                 }
                 type="password"
-                error={errors.password?.message}
-                id="fullWidth"
-                {...register("password")}
+                errors={errors.password?.message}
+                register={register}
+                valueRegister={"password"}
               />
-
-              <TextField
-                color="secondary"
-                sx={{
-                  "& input:valid + fieldset": {
-                    borderColor: "white",
-                    borderWidth: 1,
-                    borderRadius: 3,
-                    height: 75,
-                  },
-                  filter: "drop-shadow(0px 4px 4px var(--preto-opacity))",
-                  bgcolor: "var(--branco)",
-                  borderRadius: 3,
-                  height: 70,
-                  mt: 2,
-                }}
-                id="fullWidth"
-                type="password"
-                fullWidth
+              <InputTextField
                 label={
                   errors.confirmPassword?.message
                     ? errors.confirmPassword?.message
                     : "Confirme sua senha"
                 }
-                error={errors.confirmPassword?.message}
-                {...register("confirmPassword")}
+                type="password"
+                errors={errors.confirmPassword?.message}
+                register={register}
+                valueRegister={"confirmPassword"}
               />
-
               <button type="submit">Enviar</button>
               <Link to="/login">Possui conta? Faça LOGIN</Link>
             </form>
           </ConteinerFormGradient>
         </ConteinerForm>
       </ConteinerBox>
-    </Conteiner>
+    </Container>
   );
 };
 
